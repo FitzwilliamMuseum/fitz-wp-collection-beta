97e5fab0de95f1be7c248ba20464e14e6fdfe9e5
Daniel Pett <dejp3@cam.ac.uk>
2021-02-18 22:28:50 +0000

    Add license

--- views/fitzcol-artwork-figure-single.php
+++ views/fitzcol-artwork-figure-single.php
@@ -18,7 +18,6 @@
   <figcaption class="fitzcol-caption-text wp-caption-text">
     <?php if ( 'auto' == $this->get_caption_option() ): ?>
       <h3>
-
         <a href="https://collection.beta.fitz.ms/id/object/<?php esc_html_e($this->get_artwork_record()->get_id());?>"><?php esc_html_e($this->get_artwork_record()->get_accession_number());?>:<?php esc_html_e( $this->get_artwork_record()->get_title() );?></a>
       </h3>
     <?php endif; ?>
@@ -27,7 +26,7 @@
   <span class="fitzcol-copyright">
     Image rights holder: <?php esc_html_e( $this->get_artwork_record()->get_image_copyright_holder() ); ?>
     <br />
-    <a href="https://creativecommons.org/licenses/<?php esc_html_e( strtolower( $this->get_artwork_record()->get_image_license_acronym() ) ); ?>/2.0/">
+    License: <a href="https://creativecommons.org/licenses/<?php esc_html_e( strtolower( $this->get_artwork_record()->get_image_license() ) ); ?>/2.0/">
       <?php esc_html_e( $this->get_artwork_record()->get_image_license_acronym() ); ?> 2.0
     </a>
 
